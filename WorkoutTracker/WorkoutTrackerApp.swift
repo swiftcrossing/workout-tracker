ca83a3951dd2f3efd12a8c4e339a275ad312a21c
Nolan W <swiftcrossing@proton.me>
2023-08-07 23:30:54 +0900

    Initialize project

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Nolan Warner on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
